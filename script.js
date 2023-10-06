be1b544b8297ee572e912b88ee243500701d868e
Hornyák <bence.hornyak63@gmail.com>
2023-10-06 19:07:39 +0200

    dom-manipulation-call-back

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-/* function greetMe(param) {
-    console.log(`szia ${param}!`)
+console.log("hello world")
+
+function domManipulation() {
+    const rootElement = document.querySelector('#root')
+    rootElement.innerHTML = "this is done by dom manipulation"
 }
 
-greetMe("argument")
+let clickCount = 0
 
- */// A fenti függvényben található paraméter és argumentum a hivatalos nevüket jelzi! 
-
- // NaN = Not a Number
-
-/* function addTwoNumbers(num1, num2) {
-    let sum = num1 + num2
-    console.log(`the sum of two numbers (${num1}, ${num2}) is: ${sum}`)
-    return sum
+function logClick(){
+    console.log(`click: ${clickCount}`)
+    clickCount++
 }
 
-let resultOfAddTwoNumbers = addTwoNumbers(12.2, 20)
-
-console.log(`the resultOfAddTwoNumbers: ${resultOfAddTwoNumbers}`) */
-
-
-function cbExample() {
-    console.log("i am a callback function")
-}
-
-function funcExample(name, callback) {
-    console.log(`hello ${name}`)
-    callback()
-}
-
-funcExample("Bence", cbExample)
+window.addEventListener('load', domManipulation)           //eseményfigyelő
+window.addEventListener('click', logClick)
